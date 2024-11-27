17c780e753da113a1fb03bf7b0c99403c7753fc7
Henrique <rique9906@gmail.com>
2024-11-27 18:43:16 -0300

    eae fi

---
+++ Estrutura de Dados/Módulos/M07 - Grafos/Aula 01/terminologia.py
@@ -0,0 +1,54 @@
+"""
+Para que servem os Grafos?
+ - roteiros
+ - relação entre alunos e cursos
+ - links entre páginas na internet
+ - mapeamento de rotas de transporte
+ - layout de um mundo de jogo de aventura
+ - pré-requisitos entre disciplinas num curso
+ - capacidades de fluxo de comunicação de redes
+
+Características:
+ - conjunto de objetos ligados
+ - permitem ao usuario navegar de um objeto a outro
+
+ * Os grafos englobam as categorias linear, hierárquica e não ordenada das coleções como casos especiais, o grafo é a categoria mais geral de coleção.
+
+Terminologia:
+ - um grafo é o conjunto de V de vertices e um conjunto A de arestas, cada aresta conecta dois dos vertices em V.
+
+ Vertice ou Nó:
+  - origem ou destino
+ 
+ Aresta:
+  - ligação entre vertices
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Estrutura de Dados/Módulos/M07 - Grafos/Aula 03/Grafo/Lista Adjacencia/adjNode.py
@@ -0,0 +1,5 @@
+class AdjNode:
+    def __init__(self, data):
+        self.vertex = data
+        self.next = None
+        
